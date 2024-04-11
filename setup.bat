67c94e3d43ee654b1b9328f3ea026dba1330aa59
is4cx23 <00001109701159sp@al.educacao.sp.gov.br>
2024-04-11 11:32:38 -0300

    setup feito e testado

---
+++ setup.bat
@@ -0,0 +1,9 @@
+git config --global user.name "is4cx23"
+git config --global user.email "00001109701159sp@al.educacao.sp.gov.br"
+
+cd C:\Users\MAUA-MERCEDESVALENTI\
+
+git clone https://github.com/is4cx23/Caesar-Cipher.git
+cd  Caesar-Cipher
+
+code .
