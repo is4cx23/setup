90f6de9a99d66e89fa729bae70d51c2894c62c50
is4cx23 <00001109701159sp@al.educacao.sp.gov.br>
2024-05-21 10:42:28 -0300

    updates userprofile

--- setup.bat
+++ setup.bat
@@ -1,7 +1,7 @@
 git config --global user.name "is4cx23"
 git config --global user.email "00001109701159sp@al.educacao.sp.gov.br"
 
-cd C:\Users\MAUA-MERCEDESVALENTI\
+cd %userprofile%
 
 git clone https://github.com/is4cx23/Caesar-Cipher.git
 cd  Caesar-Cipher
